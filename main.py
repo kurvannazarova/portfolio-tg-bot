9ce09ac5d65fc205a2f20a9346e690f8f6965471
kurvannazarova <bekchanovagulmira7@gmail.com>
2026-05-21 16:55:06 +0500

    Refactor bot token handling and improve welcome message formatting

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import telebot
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
 
-# load_dotenv()
+load_dotenv()
 
-BOT_TOKEN = "8930768015:AAEjPp0oOmDIxSR1r5KVfQrii-aTnAg9ZT4"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalom alaykum, botga xush kelibsiz")
+	bot.reply_to(message, "Assalom alaykum👋🏻, botga xush kelibsiz!" \
+	"Men Kurvannazarova Dilmira kichik frontend dasturchiman👩🏻‍💻")
 	
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+# 	bot.reply_to(message, message.text)
 	
 bot.infinity_polling()
 
