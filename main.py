fb451833a230a6383e474bd6266a1db3e17d6ce8
kurvannazarova <bekchanovagulmira7@gmail.com>
2026-05-21 16:56:59 +0500

    Fix welcome message formatting by removing unnecessary line continuation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalom alaykum👋🏻, botga xush kelibsiz!" \
+	bot.reply_to(message, "Assalom alaykum👋🏻, botga xush kelibsiz!" 
 	"Men Kurvannazarova Dilmira kichik frontend dasturchiman👩🏻‍💻")
 	
 # @bot.message_handler(func=lambda m: True)
