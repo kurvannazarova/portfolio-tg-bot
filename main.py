50f34a83b70446cad1558f14d2c490d5595f4d3b
kurvannazarova <bekchanovagulmira7@gmail.com>
2026-05-23 17:12:28 +0500

    Enhance welcome message by adding a keyboard with options and updating the message format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
 
@@ -10,11 +11,15 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalom alaykum👋🏻, botga xush kelibsiz! Men Kurvannazarova Dilmira kichik frontend dasturchiman👩🏻‍💻." )
+	# bot.reply_to(message, "Assalom alaykum👋🏻, botga xush kelibsiz! Men Kurvannazarova Dilmira kichik frontend dasturchiman👩🏻‍💻." )
+    keyboard = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("About me")
+    btn2 = types.KeyboardButton("Contact")
+    keyboard.add(btn1, btn2)
+    bot.send_message(message.chat.id, "Assalom alaykum👋🏻, botga xush kelibsiz! Men Kurvannazarova Dilmira kichik frontend dasturchiman👩🏻‍💻.", reply_markup = keyboard )
 	
 # @bot.message_handler(func=lambda m: True)
 # def echo_all(message):
 # 	bot.reply_to(message, message.text)
 	
 bot.infinity_polling()
-
